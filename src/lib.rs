09d4c28f90fff576d73a45faf20b1437fe11c42e
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2025-05-13 20:48:32 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+    wasm_logger::init(wasm_logger::Config::default());
+
+    log::info!("Hello, slidy-scrambler!");
+}
