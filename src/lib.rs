8587fe12b183998a5147eed0076dadb238b4f052
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2025-05-13 23:59:23 +0100

    Move creation of table rows to a separate function, and add two extra scrambles

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use itertools::Itertools;
 use slidy::{
-    algorithm::as_slice::AsAlgorithmSlice,
+    algorithm::{algorithm::Algorithm, as_slice::AsAlgorithmSlice},
     puzzle::{
         color_scheme::{Black, ColorScheme, Scheme},
         coloring::Rainbow,
         label::label::SplitSquareFringe,
         puzzle::Puzzle,
-        render::{RendererBuilder, Text},
+        render::{Renderer, RendererBuilder, Text},
         scrambler::{RandomState, Scrambler},
+        sliding_puzzle::SlidingPuzzle,
     },
     solver::pdb4443::solver::Solver,
 };
@@ -29,49 +30,17 @@ pub fn main() {
     .tile_size(30.0)
     .build();
 
+    let main_table = get_element_by_id("main-table");
+    let extras_table = get_element_by_id("extras-table");
+
     for i in 0..5 {
-        let mut state = Puzzle::default();
-        RandomState.scramble(&mut state);
+        let row = make_scramble_row(i + 1, &solver, &renderer);
+        main_table.append_child(&row).unwrap();
+    }
 
-        let solution = solver.solve(&state).unwrap();
-        let scramble = solution.inverse();
-
-        let scramble_string = scramble
-            .as_slice()
-            .moves()
-            .map(|m| {
-                let mut s = m.to_string();
-                if m.amount() == 1 {
-                    s.push_str(" ");
-                }
-                s
-            })
-            .chunks(10)
-            .into_iter()
-            .map(|mut chunk| chunk.join(" "))
-            .join("\n");
-
-        let svg = renderer.render(&state).unwrap();
-
-        let tr = create_element("tr");
-
-        let col_num = create_element("td");
-        col_num.set_class_name("col-num");
-        col_num.set_inner_text(&format!("{}", i + 1));
-        tr.append_child(&col_num).unwrap();
-
-        let col_scramble = create_element("td");
-        col_scramble.set_class_name("col-scramble");
-        col_scramble.set_inner_text(&scramble_string);
-        tr.append_child(&col_scramble).unwrap();
-
-        let col_puzzle = create_element("td");
-        col_puzzle.set_class_name("col-puzzle");
-        col_puzzle.set_inner_html(&svg.to_string());
-        tr.append_child(&col_puzzle).unwrap();
-
-        let main_table = get_element_by_id("main-table");
-        main_table.append_child(&tr).unwrap();
+    for i in 0..2 {
+        let row = make_scramble_row(i + 1, &solver, &renderer);
+        extras_table.append_child(&row).unwrap();
     }
 }
 
@@ -85,4 +54,56 @@ fn get_element_by_id(id: &str) -> HtmlElement {
     let document = web_sys::window().unwrap().document().unwrap();
     let element = document.get_element_by_id(id).unwrap();
     element.dyn_into::<HtmlElement>().unwrap()
+}
+
+fn make_scramble_row(row_num: u32, solver: &Solver, renderer: &Renderer) -> HtmlElement {
+    let mut state = Puzzle::default();
+    RandomState.scramble(&mut state);
+
+    let solution = solver.solve(&state).unwrap();
+    let scramble = solution.inverse();
+
+    let scramble_string = scramble
+        .as_slice()
+        .moves()
+        .map(|m| {
+            let mut s = m.to_string();
+            if m.amount() == 1 {
+                s.push_str(" ");
+            }
+            s
+        })
+        .chunks(10)
+        .into_iter()
+        .map(|mut chunk| chunk.join(" "))
+        .join("\n");
+
+    // Double check that the scramble generates the correct state
+    {
+        let mut check_state = Puzzle::default();
+        let alg = scramble_string.parse::<Algorithm>().unwrap();
+        check_state.apply_alg(&alg);
+        assert_eq!(check_state, state, "scramble check failed");
+    }
+
+    let svg = renderer.render(&state).unwrap();
+
+    let tr = create_element("tr");
+
+    let col_num = create_element("td");
+    col_num.set_class_name("col-num");
+    col_num.set_inner_text(&row_num.to_string());
+    tr.append_child(&col_num).unwrap();
+
+    let col_scramble = create_element("td");
+    col_scramble.set_class_name("col-scramble");
+    col_scramble.set_inner_text(&scramble_string);
+    tr.append_child(&col_scramble).unwrap();
+
+    let col_puzzle = create_element("td");
+    col_puzzle.set_class_name("col-puzzle");
+    col_puzzle.set_inner_html(&svg.to_string());
+    tr.append_child(&col_puzzle).unwrap();
+
+    tr
 }
